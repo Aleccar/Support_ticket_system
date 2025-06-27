8e642e3485bab72fd46f8cfcf586e86db1d34862
Alec Strand <alexander@strand.it>
2025-06-27 15:10:22 +0200

    Added route folder and route files.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const morgan = require('morgan')
-
+require('dotenv').config()
 const app = express()
 
 
@@ -10,3 +10,8 @@ app.use(express.json())
 app.use(morgan('short'))
 
 
+PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+    console.log(`listening on port ${PORT}`)
+})
